b91ffe7b864d0921facce3930e6ac37711bbccbc
Mark Keysor <markkeysor@gmail.com>
2015-08-03 17:03:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module('maggsLashes', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'app/templates/indexTmpl.html',
+			controller: 'mainCtrl'
+		})
+
+		.when('/about', {
+			templateUrl: 'app/templates/aboutTmpl.html',
+			// controller: 'aboutCtrl'
+		})
+
+		.when('/featured-clients', {
+			templateUrl: 'app/templates/clientsTmpl.html',
+			// controller: 'clientsCtrl'
+		})
+
+		.otherwise ({
+			redirectTo: '/'
+		})
+});
+
---
+++ app/directives/footerDirective.js
@@ -0,0 +1,5 @@
+app.directive('footerDirective', function() {
+	return {
+		templateUrl: 'app/templates/footerDirective.html'
+	}
+})
